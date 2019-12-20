61fed796d02833e4fc7cdbdbd2719167ce1cb373
Philipp Schlunder <pschlunder@rapidminer.com>
2019-12-20 11:10:35 +0100

    Moved setup.py to right location and filled out mandatory information.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rm_utilities',
+	  version='1.0',
+	  description='A collection of utility function aiding the integration between RapidMiner and Python.',
+	  author='Martin Schmitz',
+	  author_email='mschmitz@rapidminer.com',
+	  license='MIT',
+	  install_requires=['pandas']
+	  )
